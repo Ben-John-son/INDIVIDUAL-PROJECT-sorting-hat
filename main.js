3986cf5648deb5bdfec469d2fbcc29928f2ba97f
Ben-John-son <greypilgrim115@gmail.com>
2023-11-04 08:51:31 -0500

    Got sort button to partiallu function.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let houses = [
   {
     id: 1,
-    studentWho: "",
+    studentWho: "Hubert",
     house: "Gryffindor",
     imageUrl: "https://www.color-hex.com/palettes/813.png",
   },
@@ -25,6 +25,15 @@ let houses = [
   },
 ];
 
+let misguidedYouth = [
+  {
+    id: 1,
+    studentWho: "Bumpus",
+    imageUrl:
+      "https://i.chzbgr.com/full/5013194240/h80B3CDC7/he-who-must-not-bean-named",
+  },
+];
+
 const renderToDom = (array) => {
   let domString = "";
   for (house of array) {
@@ -36,7 +45,9 @@ const renderToDom = (array) => {
        <div class="card-body">
        <h3>${house.studentWho}</h3>
      <footer class="card-text">${house.house}</footer>
-       <button class="deleteBtn" id="delete--${house.id}">Expel</button>
+     <div id="dBtn">
+       <button id="delete--${house.id}"class="bg-primary">Expel</button>
+       </div>
        </div>
      </div>
   
@@ -44,29 +55,51 @@ const renderToDom = (array) => {
   }
   const app = document.querySelector("#firstYearBlock");
   app.innerHTML = domString;
-  // renderToDom(domString);
 };
 renderToDom(houses);
+
+let voldyToDom = (array) => {
+  let domString = "";
+  for (misguidedYouth of array) {
+    domString += `<div class="row g-0">
+    <div class="col-md-4">
+     <img src="${misguidedYouth.imageUrl}" class="img-fluid rounded-start" alt="...">
+    </div>
+     <div class="col-md-8">
+       <div class="card-body">
+       <h3>${misguidedYouth.studentWho}</h3>
+     <footer class="card-text"></footer>
+  
+       </div>
+     </div>
+  
+ </div>`;
+  }
+  const app = document.querySelector("#voldyBlock");
+  app.innerHTML = domString;
+};
+
+// voldyToDom(misguidedYouth);
+
 // const renderToDom = (divId, content) => {
 //   const selectDiv = document.querySelector(divId);
 //   selectDiv.innerHTML = content;
 // };
 
 const studentEnter = () => {
-  let newInfo = `<div  class="card border-dark mb-3" style="max-width: 18rem;">
-  <div class="card-header">Student Name</div>
-  <div class="card-body">
-    
-  <div id="entryForm" class="mb-3">
+  let newInfo = "";
+  newInfo += `<div id="studEnter">
+    <form id="formStud">
+    <label id="nameEnter" for="name">Student Name</label>
+    <input type="text" name="name" id="name" required />
   
-  <input type="text" required class="form-control" id="student" aria-describedby="emailHelp" />
-  <div id="sort">
-  <button  id="hitIt" class="btn btn-primary mb-2" >Sort!</button>
-  </div>
-</div>;
-  </div>
- </div>`;
-  let app = document.querySelector("#formCard");
+    <button id="sorter" class="btn btn-primary mb-2" ><img
+    src="https://www.hp-lexicon.org/wp-content/uploads/2017/03/sorting-hat.png" width="100px" height="75px"
+  />Sort!</button>
+    </form>
+  
+    </div>`;
+  let app = document.querySelector("#indexForm");
   app.innerHTML = newInfo;
 };
 
@@ -77,33 +110,38 @@ appear.addEventListener("click", studentEnter);
 let createStudent = (e) => {
   e.preventDefault();
 
-  function imageFunc() {
-    if (houses.house === "Gryffindor") {
-      return "https://www.color-hex.com/palettes/813.png";
-    } else if (houses.house === "Slytherin") {
-      return "https://www.color-hex.com/palettes/17239.png";
-    } else if (houses.house === "Hufflepuff") {
-      return "https://www.color-hex.com/palettes/816.png";
-    } else {
-      return "https://www.color-hex.com/palettes/85234.png";
-    }
-  }
   let studentObj = {
     id: houses.length + 1,
-    studentWho: document.querySelector("#student").value,
+    studentWho: document.querySelector("#name").value,
     house: houses[Math.floor(Math.random() * houses.length)].house,
-    imageUrl: imageFunc(),
+    imageUrl: "https://www.color-hex.com/palettes/816.png",
   };
   console.log("sorted!");
 
   houses.push(studentObj);
+
+  if (houses[houses.length - 1].house === "Gryffindor") {
+    houses[houses.length - 1].imageUrl =
+      "https://www.color-hex.com/palettes/813.png";
+  } else if (houses[houses.length - 1].house === "Slytherin") {
+    houses[houses.length - 1].imageUrl =
+      "https://www.color-hex.com/palettes/17239.png";
+  } else if (houses[houses.length - 1].house === "Hufflepuff") {
+    houses[houses.length - 1].imageUrl =
+      "https://www.color-hex.com/palettes/816.png";
+  } else {
+    houses[houses.length - 1].imageUrl =
+      "https://www.color-hex.com/palettes/816.png";
+  }
+
   renderToDom(houses);
-  // form.reset();
+  let formRes = document.querySelector("#indexForm");
+  formRes.reset();
 };
 
-const sortBtn = document.querySelector("#formCard");
+const sortBtn = document.querySelector("#indexForm");
 
-sortBtn.addEventListener("click", createStudent);
+sortBtn.addEventListener("submit", createStudent);
 
 let slyth = document.querySelector("#slythBtn");
 let gryff = document.querySelector("#gryffBtn");
@@ -166,15 +204,40 @@ const filter4 = () => {
 
 rav.addEventListener("click", filter4);
 
-let lackeys = document.querySelector(".deleteBtn");
+let lackeys = document.querySelector("#dBtn");
+// let lackeys = document.querySelector("#delete--");
 
-let expelled = (event) => {
-  let voldArr = [];
-  if (event.target.id.includes(".deleteBtn")) {
-    voldArr.push(house);
+// const expelStudent = (event) => {
+//   let voldArr = [misguidedYouth];
+//   if (event.target.id.includes("delete")) {
+//     const id = event.target.id.split("--");
+//     const index = houses.findIndex((obj) => obj.id === Number(id));
+//     voldArr.push(houses.splice(index, 1));
+//     voldyToDom(voldArr);
+//     renderToDom(houses);
+//   }
+//   console.log("clicked");
+// };
+
+const voldArr = [];
+const expelStudent = (event) => {
+  // note that .target.id needs to be id of button. previously was not working because in card above, delete was a class, not id
+  // let voldArr = [misguidedYouth];
+  if (event.target.id.includes("delete")) {
+    const [, id] = event.target.id.split("--");
+    const index = houses.findIndex((obj) => obj.id === Number(id));
+    console.log([, id]);
+    const removed = houses.splice(index, 1);
+
+    // voldArr.push(removed.pop(index));
+    voldArr.push(removed[0]);
+    voldArr[voldArr.length - 1].imageUrl =
+      "https://i.chzbgr.com/full/5013194240/h80B3CDC7/he-who-must-not-bean-named";
+
+    voldyToDom(voldArr);
+    renderToDom(houses);
   }
-  renderToDom(".voldy");
   console.log("clicked");
 };
 
-// lackeys.addEventListener("click", expelled);
+lackeys.addEventListener("click", expelStudent);
